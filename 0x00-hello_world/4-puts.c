2a905a9e3a1349d169e6ae06179fc5901f67f7f2
kmazengia <kefecoder@gmail.com>
2022-09-09 00:41:19 -0700

    finaly succedded

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-  /**
-   *main -prints the string in the put function
-   *Description: using the main function this program prints
-   *"Programming is like  building a multilingual puzzle
-   *return 0        
-   */
+/**
+   * main -prints the string in the put function
+   * Description: using the main function this program prints
+   * "Programming is like  building a multilingual puzzle
+   * return o
+   * Description: returns 0 if success
+ **/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+}
