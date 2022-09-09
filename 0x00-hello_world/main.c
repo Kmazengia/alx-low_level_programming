6cf25a23aba7ada995f2cd68df3da5add1e58ef7
kmazengia <kefecoder@gmail.com>
2022-09-08 17:01:06 -0700

    pre processor

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,16 +1,8 @@
-nclude <stdio.h>
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+include <stdio.h>
+/*main - Entry point
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
-
 	    return (0);
-
 }
