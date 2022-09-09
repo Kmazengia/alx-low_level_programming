27c493bec079b1d41b8a987ad081a9806927ed03
kmazengia <kefecoder@gmail.com>
2022-09-09 01:49:03 -0700

    put command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,13 @@
    * main -prints the string in the put function
    * Description: using the main function this program prints
    * "Programming is like  building a multilingual puzzle
-   * return o
-   * Description: returns 0 if success
- **/
+   * Return: returns 0 if success
+ */
 int main(void)
 {
+  /**
+   *main is the first function to be excuted.
+   */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
