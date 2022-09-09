61202203d2ebe17d2f5670989caa2be65f03bd2e
kmazengia <kefecoder@gmail.com>
2022-09-08 23:50:48 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,6 +11,6 @@ int main(void)
 	/**
 	 * The main function from which the excution begins
 	 **/ 
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
