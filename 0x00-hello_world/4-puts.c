e585177bc71d4b7d61cede0430fc0b0997b3ecb2
kmazengia <kefecoder@gmail.com>
2022-09-08 23:28:52 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ *   * Description: main - prints out a string to stdout
+ *
+ *     *Return: 0 if success
+ *
+ *       */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
+}
