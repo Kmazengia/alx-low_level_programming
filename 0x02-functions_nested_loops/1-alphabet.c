170e14d2f03feeb481dd0cb614d7850dbf562730
kmazengia <kefecoder@gmail.com>
2022-09-13 19:34:51 -0700

    alpabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *
+ * main is the initial excution line
  *
  *
  */
 void print_alphabet(void)
 {
 char c;
-for(c = "a"; C<="z": c++){
+for (c = "a"; C <= "z" : c++)
+{
 	_putchar(c);
 }
 _putchar('\n');
