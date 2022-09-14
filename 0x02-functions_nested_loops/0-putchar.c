bebae8fd83df7b23012ff449c1d8ad382e830d9f
kmazengia <kefecoder@gmail.com>
2022-09-13 18:40:21 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ *
+ *	* main - Entry point
+ *
+ *
+ *
+ *	* Return: Always success
+ *
+ *
+ */
+int main(void)
+{
+	       _putchar('_');
+
+	       _putchar('p');
+
+	       _putchar('u');
+
+	       _putchar('t');
+
+	       _putchar('c');
+
+	       _putchar('h');
+
+	       _putchar('a');
+
+	       _putchar('r');
+
+	       _putchar(''\n);
+
+	       return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
