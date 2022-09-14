73ee1df88fd0c2efcd2c996d2e6099d0a0f82cc9
kmazengia <kefecoder@gmail.com>
2022-09-13 17:08:41 -0700

    commiting

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void is_lower(int c);
+void is_alpha(int c);
+int print_sign(int n)
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_times_table(int n);
+void num(int r, int n);
+#endif
