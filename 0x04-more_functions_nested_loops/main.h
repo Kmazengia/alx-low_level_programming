98fdbda3b12d6cc49f806924b5e581047971aeb4
kmazengia <kefecoder@gmail.com>
2022-09-15 16:37:40 -0700

    fun and loop

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -15,9 +13,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-
 #endif
