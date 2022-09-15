f87c4f69aca30a7a677cb97e9a85427fa382ada3
kmazengia <kefecoder@gmail.com>
2022-09-14 17:32:02 -0700

    function commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,18 @@
 #include "main.h"
 /**
- *
- *print_alphabet_x10 - Prints the alphabet 10 times.
- *
- * Return: Always 0 (Success)
- *
- *
- */
+* print_alphabet_x10 - Prints the alphabet 10 times.
+* Return: Always 0 (Success)
+*/
 
 void print_alphabet_x10(void)
-
 {
-	int a, b;
-
-	for (a = 0; a <= 9; a++)
+int a, b;
+for (a = 0; a <= 9; a++)
+{
+	for (b = 97; b <= 122; b++)
 	{
-		for (b = 97; b  = 122; b++)
-		{
 		_putchar(b);
-		}
-	_putchar('\n');
 	}
+	_putchar('\n');
+}
 }
