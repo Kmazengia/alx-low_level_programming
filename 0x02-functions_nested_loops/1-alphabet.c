7d8e11c7206a79b7ffa0c74e748554f49dde2565
kmazengia <kefecoder@gmail.com>
2022-09-14 17:06:28 -0700

    0x02 function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,15 @@
 #include "main.h"
 /**
- * main is the initial excution line
+ *	main is the initial excution line
  *
  *
  */
-void print_alphabet(void)
-{
-	/**
-	 *main function begines here
-	 *Description is description
-	 */
-char c;
-for (c = "a"; C <= "z" : c++)
-{
-	_putchar(c);
-}
-_putchar('\n');
-}
+void print_alphabet(void)/*this is main function*/
+	{
+	char;
+	for (c = "a"; C <= "z" : c++)
+		{
+		_putchar(c);
+		}
+	_putchar('\n');
+	}
