855bed1253ab74eed8750168b9afad6cf974a2df
kmazengia <kefecoder@gmail.com>
2022-09-15 16:24:46 -0700

    functions

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,5 +15,9 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif
